b02c3e20e5fc8dd1619a2aee68d9a084bddbf21f
ajain-us <adityajain_us@icloud.com>
2024-09-07 19:30:43 -0400

    made a lot of work today, set up basic framework for adding and editing shows. I need to finish these and add the SQL queries. I also need to add functionality for the delete button but that will take two seconds

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QLineEdit, QHBoxLayout, QMainWindow, QStyleFactory, QTableWidgetItem, QTableWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QLineEdit, QHBoxLayout, QMainWindow, QStyleFactory, QTableWidgetItem, QTableWidget, QComboBox
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIntValidator
 import sys, pyodbc
 
 
@@ -189,7 +190,7 @@ class InfoWindow(QWidget):
         self.logoutButton = QPushButton("Logout")
         self.addShow = QPushButton("Add")
         self.deleteShow = QPushButton("Delete")
-        self.saveButton = QPushButton("Save")
+        self.editButton = QPushButton("Edit")
         self.showsTable = QTableWidget()
         
         self.shows = []
@@ -204,7 +205,7 @@ class InfoWindow(QWidget):
         #Item(show.Title, show.WatchStatus, show.Rating, show.TotalEpisodes, show.CurrentEpisode)
 
         for show in showsInfo:
-            self.shows.append({Item(show.Title, show.WatchStatus, show.Rating, show.TotalEpisodes, show.CurrentEpisode)})
+            self.shows.append(Item(show.Title, show.WatchStatus, show.Rating, show.TotalEpisodes, show.CurrentEpisode))
             self.showsTable.setRowCount(self.showsTable.rowCount() + 1)
             tableWidgets = [QTableWidgetItem(show.Title),QTableWidgetItem(show.WatchStatus), QTableWidgetItem(str(show.Rating)), QTableWidgetItem(str(show.CurrentEpisode) + "/" + str(show.TotalEpisodes))]
             for aspect in tableWidgets:
@@ -215,19 +216,12 @@ class InfoWindow(QWidget):
             self.showsTable.setItem(self.showsTable.rowCount()-1, 1, tableWidgets[1])
             self.showsTable.setItem(self.showsTable.rowCount()-1, 2, tableWidgets[2])
             self.showsTable.setItem(self.showsTable.rowCount()-1, 3, tableWidgets[3])
-
-
-        
-
-
         #tableWidget.cellDoubleClicked.connect(on_cell_double_clicked) and this function takes in (row: int, column: int)
-
-
         middleLayout = QHBoxLayout()
         buttonLayout = QVBoxLayout()
         buttonLayout.addWidget(self.addShow)
         buttonLayout.addWidget(self.deleteShow)
-        buttonLayout.addWidget(self.saveButton)
+        buttonLayout.addWidget(self.editButton)
         buttonLayout.addWidget(self.logoutButton)
 
         middleLayout.addWidget(self.showsTable)
@@ -236,20 +230,121 @@ class InfoWindow(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(self.nameLabel)
         layout.addLayout(middleLayout)
-        
-        
+
+
+        self.editButton.clicked.connect(self.editFunction)
 
         self.setLayout(layout)
 
+    def addShow(self):
+
+
+    def editFunction(self):
+        print("well we are on row " + str(self.showsTable.currentRow()))
+        if(self.showsTable.currentRow() > 0):
+            self.temp = editShowWindow(self.shows[self.showsTable.currentRow()])
+
+
 class addShowWindow(QWidget):
-    def __init__(self, name, *args, **kwargs):
+    def __init__(self, name, table, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.exitButton = QPushButton("add")
+
+        self.statusLabel = QLabel("Edit")
         self.titleLabel = QLabel("Title:")
-        self.statusLabel = QLabel("Watch Status:")
+        self.watchStatusLabel = QLabel("Watch Status:")
         self.ratingLabel = QLabel("Rating:")
-        self.totalEpisodesLabel = QLabel("Total Episodes:")
-        self.currentEpisodeLabel = QLabel("Current Episode:")
+        self.episodesLabel = QLabel("Episode: ")
+        self.tepisodeLabel = QLabel("Total Episodes: ")
+
+        self.titleBox = QLineEdit()
+        self.watchStatusDrop = QComboBox()
+        self.watchStatusDrop.addItems(['P','W','C'])
+        self.ratingDrop = QComboBox()
+        self.ratingDrop.addItems(['1','2','3','4','5','6','7','8','9','10'])
+        self.episodeBox = QLineEdit()
+        self.tepisodeBox = QLineEdit()
+        
+
+        labelsLayout = QVBoxLayout()
+        labelsLayout.addWidget(self.titleLabel)
+        labelsLayout.addWidget(self.watchStatusLabel)
+        labelsLayout.addWidget(self.ratingLabel)
+        labelsLayout.addWidget(self.episodesLabel)
+        labelsLayout.addWidget(self.tepisodeLabel)
+
+        editablesLayout = QVBoxLayout()
+        editablesLayout.addWidget(self.titleBox)
+        editablesLayout.addWidget(self.watchStatusDrop)
+        editablesLayout.addWidget(self.ratingDrop)
+        editablesLayout.addWidget(self.episodeBox)
+        editablesLayout.addWidget(self.tepisodeBox)
+
+        itemsLayout = QHBoxLayout()
+        itemsLayout.addLayout(labelsLayout)
+        itemsLayout.addLayout(editablesLayout)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.statusLabel)
+        layout.addLayout(itemsLayout)
+        layout.addWidget(self.exitButton)
+
+        self.setLayout(layout)
+        self.show()
+
+class editShowWindow(QWidget):
+    def __init__(self, item, name, table, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.exitButton = QPushButton("exit")
+
+        self.statusLabel = QLabel("Edit")
+        self.titleLabel = QLabel("Title:")
+        self.watchStatusLabel = QLabel("Watch Status:")
+        self.ratingLabel = QLabel("Rating:")
+        self.episodesLabel = QLabel("Episode: ")
+
+        self.titleBox = QLineEdit()
+        self.titleBox.setText(item.title)
+        self.watchStatusDrop = QComboBox()
+        self.watchStatusDrop.addItems(['P','W','C'])
+        if(item.status == 'P'):
+            self.watchStatusDrop.setCurrentIndex(0)
+        elif(item.status == 'W'):
+            self.watchStatusDrop.setCurrentIndex(1)
+        else:
+            self.watchStatusDrop.setCurrentIndex(2)
+        self.ratingDrop = QComboBox()
+        self.ratingDrop.addItems(['1','2','3','4','5','6','7','8','9','10'])
+        self.ratingDrop.setCurrentIndex(item.rating-1)
+        self.episodeBox = QLineEdit()
+        self.episodeBox.setText(str(item.currentEpisode))
+        self.episodeBox.setValidator(QIntValidator(0, int(item.totalEpisodes)+1))
+
+        labelsLayout = QVBoxLayout()
+        labelsLayout.addWidget(self.titleLabel)
+        labelsLayout.addWidget(self.watchStatusLabel)
+        labelsLayout.addWidget(self.ratingLabel)
+        labelsLayout.addWidget(self.episodesLabel)
+
+        editablesLayout = QVBoxLayout()
+        editablesLayout.addWidget(self.titleBox)
+        editablesLayout.addWidget(self.watchStatusDrop)
+        editablesLayout.addWidget(self.ratingDrop)
+        editablesLayout.addWidget(self.episodeBox)
+
+        itemsLayout = QHBoxLayout()
+        itemsLayout.addLayout(labelsLayout)
+        itemsLayout.addLayout(editablesLayout)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.statusLabel)
+        layout.addLayout(itemsLayout)
+        layout.addWidget(self.exitButton)
+
+        self.setLayout(layout)
+        self.show()
 
 class Item():
     def __init__(self, title, status, rating, totalEpisodes, currentEpisode):
